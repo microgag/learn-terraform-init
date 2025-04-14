a3365f36829f7a47bd5b7a2acb8a237e73aca0fa
microgag <16010631+microgag@users.noreply.github.com>
2025-04-14 12:07:42 +0800

    Update terraform.tf
    insert ansible providers

--- terraform.tf
+++ terraform.tf
@@ -12,6 +12,10 @@ terraform {
       source  = "hashicorp/random"
       version = "3.6.0"
     }
+    ansible = {
+      source = "ansible/ansible"
+      version = "1.3.0"
+    }
   }
   /* Uncomment this block to use Terraform Cloud for this tutorial
   cloud {
